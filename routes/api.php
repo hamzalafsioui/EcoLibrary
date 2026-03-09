e22c7278ff6b0e434fa29578892be6ec8bb21ceb
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-09 18:01:58 +0000

    add login | register | logout | Protect book routes with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
+use App\Http\Controllers\Api\BookController;
+use Symfony\Component\HttpFoundation\Request;
 
-Route::apiResource('books', BookController::class);
+// Route::post('/tokens/create', function(Request $request){
+//     $request->validate([
+//         'token_name' => 'required|string|max:255'
+//     ]);
+
+//     $token = $request->user()->createToken($request->token_name);
+
+//     return ['token' => $token->plainTextToken];
+// });
+
+Route::post("login", [AuthController::class, "login"]);
+Route::post("register", [AuthController::class, "register"]);
+Route::post("logout", [AuthController::class, "logout"]);
+
+// Protect book routes with Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('books', BookController::class);
+});
