886d7986b8546ee3d11439f8d1a7fea9713e0207
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-10 10:00:59 +0000

    add_details_to_books_table

---
+++ database/migrations/2026_03_09_183357_add_details_to_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
+            $table->integer('views_count')->default(0);
+            $table->boolean('is_available')->default(true);
+            $table->integer('total_count')->default(1);
+            $table->integer('degraded_count')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+};
